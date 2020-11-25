9e104a2d3672cc512179f89415adff94f56e487c
Gutu Galuppo <galuppodev@gmail.com>
2020-11-25 16:19:46 +0100

    first commit

---
+++ src/JumpingLettersClass.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react'
+import JumpingLetters from './JumpingLetters'
+
+export default class JumpingLettersClass extends Component {
+
+	componentDidMount() {
+		this.fillLists();
+	}
+
+	fillLists = () => {
+		var letters = document.querySelectorAll('.letter');
+		const random = (max, min) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+		console.log(letters)
+
+		let scl = [];
+		let avoColorList = [];
+
+		letters.forEach(() => {
+			scl.push(random(10, 20));
+			avoColorList.push([random(30, 60), random(0, 10), random(200, 30)]);
+		});
+
+		const setColor = (el, r, g, b) => {
+			el.style.color = `rgb(${r},${g},${b})`
+		}
+
+		letters.forEach((letter, index) => {
+			letter.addEventListener('mouseenter', () => {
+				letter.classList.add('isJumping');
+				letter.classList.add('popUp');
+
+				const newColorValueR = (avoColorList[index][0]);
+				const newColorValueG = (avoColorList[index][1]);
+				const newColorValueB = (avoColorList[index][2]);
+
+				setColor(letter, (255 - newColorValueR), (255 - newColorValueG), (255 - newColorValueB));
+
+				setTimeout(() => {
+					letter.classList.remove('isJumping')
+					letter.classList.add('popUp');
+
+					setColor(letter, 255, 255, 255)
+				}, 2000)
+			})
+		});
+	}
+
+
+
+	render() {
+		// const text = `Be water, my friend!`;
+		// const lettersArray = text.split('')
+		return (
+			<div class="box">
+				<JumpingLetters/>
+				<div class="inner">
+					{/* {lettersArray.map((letter, id) => <div key={id} className="letter">{letter}</div>)} */}
+					<div className="letter" data-title="&ldquo;">&ldquo;</div>
+					<div className="letter" data-title="B">B</div>
+					<div className="letter" data-title="e">e</div>
+					<div className="letter" data-title="&nbsp;">&nbsp;</div>
+					<div className="letter" data-title="w">w</div>
+					<div className="letter" data-title="a">a</div>
+					<div className="letter" data-title="t">t</div>
+					<div className="letter" data-title="e">e</div>
+					<div className="letter" data-title="r">r</div>
+					<div className="letter" data-title=",">,</div>
+					<div className="letter" data-title="&nbsp;">&nbsp;</div>
+					<br />
+					<div className="letter" data-title="&nbsp;">&nbsp;</div>
+					<div className="letter" data-title="&nbsp;">&nbsp;</div>
+					<div className="letter" data-title="&nbsp;">&nbsp;</div>
+					<div className="letter" data-title="&nbsp;">&nbsp;</div>
+					<div className="letter" data-title="&nbsp;">&nbsp;</div>
+					<div className="letter" data-title="m">m</div>
+					<div className="letter" data-title="y">y</div>
+					<div className="letter" data-title="&nbsp;">&nbsp;</div>
+					<div className="letter" data-title="f">f</div>
+					<div className="letter" data-title="r">r</div>
+					<div className="letter" data-title="i">i</div>
+					<div className="letter" data-title="e">e</div>
+					<div className="letter" data-title="n">n</div>
+					<div className="letter" data-title="d">d</div>
+					<div className="letter" data-title="!">!</div>
+					<div className="letter" data-title="&rdquo;">&rdquo;</div>
+					<br />
+				</div>
+			</div>
+		)
+	}
+}
